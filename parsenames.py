4d17b6d5ea91b944fc6335e02287338ce14b3439
Toni Prada <toniprada@gmail.com>
2014-10-29 11:59:39 +0100

    remove names with less than 4 letters

--- parsenames.py
+++ parsenames.py
@@ -1,6 +1,8 @@
 from os import listdir
 from os.path import isfile, join
 
+NAME_MIN_LENGTH = 3
+
 def get_folder_files(folder, extension):
     return [join(folder,f) for f in listdir(folder)
         if isfile(join(folder,f)) and f.endswith(extension)]
@@ -11,6 +13,7 @@ filenames = get_folder_files('names', '.txt')
 for fn in filenames:
     with open(fn) as f:
         names = [line.split(',')[0] for line in f]
+        names = [n for n in names if len(n)>NAME_MIN_LENGTH]
         names_set.update(names)
 
 print len(names_set), 'unique names'
