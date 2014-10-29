847a8743c80d2ce1ac1509c9e2ba4c63b63296ab
Toni Prada <toniprada@gmail.com>
2014-10-29 11:03:24 +0100

    dataset extracted

---
+++ README.md
@@ -0,0 +1,11 @@
+# names-dataset
+
+A set of US Names from the last 130 years.
+
+## Data source
+
+The data can be found at the [US Official Social Security Website](http://www.ssa.gov/oact/babynames/limits.html) with the data sliced by year and with additional info. What I wanted (and maybe you) is a list of possible names for US people.
+
+## Rerun
+
+The data is already available on the project at `names-dataset.txt`, but if you want to rerun the script just put the downloaded data at `names` folder and execute `parsenames.py`.
---
+++ parsenames.py
@@ -0,0 +1,21 @@
+from os import listdir
+from os.path import isfile, join
+
+def get_folder_files(folder, extension):
+    return [join(folder,f) for f in listdir(folder)
+        if isfile(join(folder,f)) and f.endswith(extension)]
+
+print 'starting...'
+names_set = set()
+filenames = get_folder_files('names', '.txt')
+for fn in filenames:
+    with open(fn) as f:
+        names = [line.split(',')[0] for line in f]
+        names_set.update(names)
+
+print len(names_set), 'unique names'
+with open('names-dataset.txt', 'w+') as f:
+    f.write('\n'.join(names_set).lower())
+print 'file written'
+print '...finished'
+
