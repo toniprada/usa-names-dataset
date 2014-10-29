4e559c505beece7eb91dad699e40e3c59401fa14
Toni Prada <toniprada@gmail.com>
2014-10-29 12:19:13 +0100

    min year is now 1960. min freq is 10

--- parsenames.py
+++ parsenames.py
@@ -2,6 +2,7 @@ from os import listdir
 from os.path import isfile, join
 
 NAME_MIN_LENGTH = 3
+NAME_MIN_FREQ = 10
 
 def get_folder_files(folder, extension):
     return [join(folder,f) for f in listdir(folder)
@@ -12,8 +13,9 @@ names_set = set()
 filenames = get_folder_files('names', '.txt')
 for fn in filenames:
     with open(fn) as f:
-        names = [line.split(',')[0] for line in f]
-        names = [n for n in names if len(n)>NAME_MIN_LENGTH]
+        data = [line.split(',') for line in f]
+        names = [d[0] for d in data if int(d[2])>NAME_MIN_FREQ] # min frencuency filter
+        names = [n for n in names if len(n)>NAME_MIN_LENGTH] # name length filter
         names_set.update(names)
 
 print len(names_set), 'unique names'
